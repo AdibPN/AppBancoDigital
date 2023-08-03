04f5bbb66ca6a764a0730bd89d4a55607cd34732
AdibPN <apaulinineto@gmail.com>
2023-08-03 14:59:36 -0300

    atualiza

--- AppBancoDigital/AppBancoDigital/View/TelaInicial.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/TelaInicial.xaml.cs
@@ -15,19 +15,20 @@ namespace AppBancoDigital.View
         public TelaInicial()
         {
             InitializeComponent();
-            txt_correntista.Text = App.DadosCorrentista.Nome;
+            NavigationPage.SetHasNavigationBar(this, false);
+            img_transferir2.Source = ImageSource.FromResource("AppBancoDigital.Image.img_transferir2.PNG");
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            /*Navigation.PushAsync(new View.Pix.EnviarPix());*/
+        //private void Button_Clicked(object sender, EventArgs e)
+        //{
+        //    /*Navigation.PushAsync(new View.Pix.EnviarPix());*/
 
-        }
+        //}
 
-        private void Button_Clicked_1(object sender, EventArgs e)
-        {
-            /*Navigation.PushAsync(new View.Pix.ReceberPix());*/
+        //private void Button_Clicked_1(object sender, EventArgs e)
+        //{
+        //    /*Navigation.PushAsync(new View.Pix.ReceberPix());*/
 
-        }
+        //}
     }
 }
