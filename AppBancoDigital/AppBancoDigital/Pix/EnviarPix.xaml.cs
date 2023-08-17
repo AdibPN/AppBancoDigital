476d311506b713a6d4b5ac172fa5d0a365f0dfda
AdibPN <apaulinineto@gmail.com>
2023-08-17 14:55:52 -0300

    pix

---
+++ AppBancoDigital/AppBancoDigital/Pix/EnviarPix.xaml.cs
@@ -0,0 +1,34 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.Pix
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnviarPix : ContentPage
+    {
+        public EnviarPix()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string teste = "Chave da Transferência: 46753387803 ";
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(teste, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode qRCode = new PngByteQRCode(qrCodeData);
+            byte[] qrCodeBytes = qRCode.GetGraphic(20);
+            img_qrcode.Source = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
+
+        }
+    }
+}
