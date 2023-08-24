33f1cc61686777b4bbacb33c21706f0f79288055
AdibPN <apaulinineto@gmail.com>
2023-08-24 14:55:01 -0300

    atualiza

--- AppBancoDigital/AppBancoDigital/View/TelaInicial.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/TelaInicial.xaml.cs
@@ -19,9 +19,22 @@ namespace AppBancoDigital.View
             usuario.Source = ImageSource.FromResource("AppBancoDigital.Image.usuario.png");
             img_transferir2.Source = ImageSource.FromResource("AppBancoDigital.Image.img_transferir2.PNG");
             img_pix.Source = ImageSource.FromResource("AppBancoDigital.Image.109617.png");
+            img_code.Source = ImageSource.FromResource("AppBancoDigital.Image.109617.png");
             LOGOTIPO.Source = ImageSource.FromResource("AppBancoDigital.Image.Logo.PNG");
-            mudar.Source = ImageSource.FromResource("AppBancoDigital.img_mudar.Logo.PNG");
+            //mudar.Source = ImageSource.FromResource("AppBancoDigital.img_mudar.Logo.PNG");
             
+        }
+
+
+        private void img_pix_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pix.EnviarPix());
+        }
+
+        private void img_pix_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pix.PixExibirQrCode());
+
         }
 
         //private void Button_Clicked(object sender, EventArgs e)
